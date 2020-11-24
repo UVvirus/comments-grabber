e5c054bd104e4d258626adfdcd7c541e4e7e552e
yuvarajan <yuvasyuvarajan@gmail.com>
2020-11-24 12:41:46 +0530

    explanation

--- main.py
+++ main.py
@@ -15,12 +15,27 @@ tst=browser.find_element(By.TAG_NAME("head")).send_keys(Keys.F12)
 def page_source():
     try:
         driver=webdriver.Chrome("/usr/local/bin/chromedriver")
-       # url="scanme.nmap.org"
-        driver.get("http://scanme.nmap.org")
+        url="scanme.nmap.org"
+        driver.get(url)
         soup=BeautifulSoup(driver.page_source,'html.parser')
-        #soup.find_all("<a>")
+
         driver.quit()
         for comments in soup.findAll(text=lambda text: isinstance(text, Comment)):
+            """""
+            The condition in our case is,
+
+            3.1.Firstly
+            we
+            don't check everything only the clear, plain English text and inside tags, and/or 
+            whatever string there is. That's text =
+
+            3.2.The text also hasa condition, it doesn't take any text, 
+            only if a lambda function returns True, 
+            i.e. fulfills the condition of the lambda.
+
+            3.3.The lambda condition is that it has to be an instance 
+            of Comment meaning only if it's a Comment it will return True.
+            """""
             comments.extract()
             print(comments)
     except NoSuchElementException as e:
